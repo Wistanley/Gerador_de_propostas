84627e94650de53fe935fe6931cf904e5c53c369
Wistanley <wistanleyoliveira@gmail.com>
2023-09-22 19:57:33 -0300

    .

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Login from './components/Login';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './components/Dashboard';  // Adicione esta linha
+import Dashboard from './components/dashboard';  // Adicione esta linha
 
 function App() {
   return (
